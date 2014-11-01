f572f69a5a8d95be11626c0250a01576ee4c48a7
Loriah Pope <loriahpope@172-16-92-135.dynapool.nyu.edu>
2014-11-01 14:53:16 -0400

    added list and create views

--- grocery/routes/index.js
+++ grocery/routes/index.js
@@ -1,9 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+var List = mongoose.model('List');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/list', function(req, res) {
+	List.find(function(err, list, count) {
+		res.render( 'list', {
+			list: list
+		});
+	});
+});
+
+router.get('/list/create', function(req, res) {
+  res.render('create');
+});
+
+router.post('/list/create', function(req, res) {
+	console.log(req.body.list);
+	new List({
+		name: req.body.list,
+		createdBy: req.user,
+		items: [],
+		updated_at : Date.now()
+	}).save(function(err, list, count){
+		console.log('buy these things', list, count, err);
+		res.redirect('/list');
+	});
 });
 
 module.exports = router;
